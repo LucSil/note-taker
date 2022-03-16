1eeb769b41ec90e8361153dc8f3d921ab8f1180f
LucSil <luciansilva351@gmail.com>
2022-03-16 09:04:55 +0100

    add custom event to pass data from child-to-parent

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,18 @@
     <div class="grid grid-cols-2 place-self-center">
       <div id="notes">
         <h1>Notes</h1>
+        <div v-for="note in notes" :key="note">
+          {{ note }}
+        </div>
       </div>
       <div id="timestamps">
         <h1>Timestamps</h1>
+        <div v-for="timestamp in timestamps" :key="timestamp">
+          {{ timestamp }}
+        </div>
       </div>
     </div>
-    <NoteEntry :placeholder="placeholder" />
+    <NoteEntry @add-note="addNote" :placeholder="placeholder" />
   </div>
 </template>
 
@@ -27,6 +33,12 @@ export default {
       timestamps: [],
       placeholder: "Enter a note",
     };
+  },
+  methods: {
+    addNote(e) {
+      this.notes.push(e.note);
+      this.timestamps.push(e.timestamp);
+    },
   },
 };
 </script>
--- src/components/NoteEntry.vue
+++ src/components/NoteEntry.vue
@@ -5,20 +5,41 @@
   >
     <input
       type="text"
+      @keypress.enter="monitorEnterKey"
+      v-model="input"
       :placeholder="placeholder"
       class="px-5 py-3 w-full outline-none border-4 border-60% bg-gray-300 text-[#252525] rounded-md"
     />
   </div>
-  <div class="grid place-self-center">
+  <div v-if="error">
+    <p>Input field cannot be empty !</p>
+  </div>
+  <!-- <div class="grid place-self-center">
     <p class="text-accCol-B text-lg">
       Note Count: <span class="font-extrabold">3</span>
     </p>
-  </div>
+  </div> -->
 </template>
 
 <script>
 export default {
   name: "NoteEntry",
   props: ["placeholder"],
+  emits: ["add-note"],
+  data() {
+    return {
+      input: "",
+      error: false,
+    };
+  },
+  methods: {
+    monitorEnterKey() {
+      this.$emit("add-note", {
+        note: this.input,
+        timestamp: new Date().toLocaleString(),
+      });
+      this.input = "";
+    },
+  },
 };
 </script>
