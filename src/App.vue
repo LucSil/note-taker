a12cdad3e0d8ac525e731eaa5acb2871a28aea24
LucSil <luciansilva351@gmail.com>
2022-03-17 11:45:07 +0100

    use function in computed to access store data; functionality added

--- src/App.vue
+++ src/App.vue
@@ -6,45 +6,39 @@
     <div class="grid grid-cols-2">
       <div id="notes" class="place-self-start">
         <strong>Notes</strong>
-        <div v-for="note in notes" :key="note">
+        <div v-for="note in notes" :key="note" class="text-left">
           {{ note }}
         </div>
       </div>
       <div id="timestamps" class="place-self-end">
         <strong>Timestamps</strong>
-        <div v-for="timestamp in timestamps" :key="timestamp">
+        <div
+          v-for="timestamp in timestamps"
+          :key="timestamp"
+          class="text-right"
+        >
           {{ timestamp }}
         </div>
       </div>
     </div>
-    <NoteEntry @add-note="addNote" :placeholder="placeholder" />
-    <div class="grid place-self-center">
-      <p class="text-accCol-A text-lg">
-        Notes : <span class="font-extrabold">{{ noteCount }}</span>
-      </p>
-    </div>
+    <NoteEntry />
+    <NoteCount />
   </div>
 </template>
 
 <script>
 import NoteEntry from "./components/NoteEntry.vue";
+import NoteCount from "./components/NoteCount.vue";
 
 export default {
   name: "App",
-  components: { NoteEntry },
-  data() {
-    return {
-      notes: [],
-      timestamps: [],
-      noteCount: 0,
-      placeholder: "Enter a note",
-    };
-  },
-  methods: {
-    addNote(e) {
-      this.notes.push(e.note);
-      this.timestamps.push(e.timestamp);
-      this.noteCount++;
+  components: { NoteEntry, NoteCount },
+  computed: {
+    notes() {
+      return this.$store.getters.getNotes;
+    },
+    timestamps() {
+      return this.$store.getters.getTimestamps;
     },
   },
 };
