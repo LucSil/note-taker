e3f87f4c01e10260af099de21900abe1114e9ad9
LucSil <luciansilva351@gmail.com>
2022-03-17 11:26:28 +0100

    implement mutations and actions; make store accessible through main.js

--- src/store.js
+++ src/store.js
@@ -7,6 +7,24 @@ const store = createStore({
       timestamps: [],
     };
   },
+  mutations: {
+    addNote(state, payload) {
+      let newNote = payload;
+      state.notes.push(newNote);
+    },
+    addTimestamp(state, payload) {
+      let newTimestamp = payload;
+      state.timestamps.push(newTimestamp);
+    },
+  },
+  actions: {
+    addNote(context, payload) {
+      context.commit("addNote", payload);
+    },
+    addTimestamp(context, payload) {
+      context.commit("addTimestamp", payload);
+    },
+  },
 });
 
 export default store;
