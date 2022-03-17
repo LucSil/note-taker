e6fe28ec482644a09f11dc7c9f8f8370892534ff
LucSil <luciansilva351@gmail.com>
2022-03-17 11:29:15 +0100

    create NoteCount.vue

---
+++ src/components/NoteCount.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="grid place-self-center">
+    <p class="text-accCol-A text-lg">
+      Notes : <span class="font-extrabold">{{ noteCount }}</span>
+    </p>
+  </div>
+</template>
